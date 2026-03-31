a3ba4b2aff0585a96572e41b34e739ea49c98364
Mark Alan Hix <markahix@gmail.com>
2026-03-31 16:22:24 -0400

    update Makefile for AGIMUS incorporation

---
+++ obj/filetypes.d
@@ -0,0 +1,6 @@
+obj/filetypes.o: src/filetypes.cpp include/filetypes.h \
+ include/utilities.h
+
+include/filetypes.h:
+
+include/utilities.h:
---
+++ obj/main.d
@@ -0,0 +1,8 @@
+obj/main.o: src/main.cpp include/utilities.h include/filetypes.h \
+ include/utilities.h
+
+include/utilities.h:
+
+include/filetypes.h:
+
+include/utilities.h:
